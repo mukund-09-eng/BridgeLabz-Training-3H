53315472fad042592c1fd0732a00cebcb4b15ba9
mukund-09-eng <mukundbansal000l@gmail.com>
2025-09-04 10:58:49 +0530

    [Mukund]Added File Student

---
+++ Java_Class/Level2/Student.java
@@ -0,0 +1,34 @@
+package Bridgelabz.oops;
+
+class Student {
+    private String name;
+    private int rollNumber;
+    private int marks;
+
+    public Student(String name, int rollNumber, int marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    public String calculateGrade() {
+        if (marks >= 90) return "A+";
+        else if (marks >= 75) return "A";
+        else if (marks >= 60) return "B";
+        else if (marks >= 40) return "C";
+        else return "Fail";
+    }
+
+    public void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Mukund", 101, 85);
+        s1.displayDetails();
+    }
+}
+
